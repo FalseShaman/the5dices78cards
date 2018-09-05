8e15ee875db974d6a3c3b6a9dc2ff79de8841619
Professional <Professional@User-PC.Dlink>
2018-09-06 00:21:39 +0600

    views and model list

--- controller/Controller.php
+++ controller/Controller.php
@@ -5,7 +5,10 @@
         public $path;
         
         public $view = 'home';
+        private $viewlList = ['home', 'lost'];
+        
         public $model = 'base';
+        private $modelList = ['home', 'lost'];
                 
         function __construct($path = '/') {
             $this->path = $path;
@@ -14,14 +17,15 @@
         public function createRoute() {
             $parts = explode('/', $this->path);
             
-            if (isset($parts[1]) && $parts[1] > '') {
+            if (isset($parts[1]) && in_array($parts[1], $this->viewlList)) {
                 $this->view = $parts[1];
             }
-            if (isset($parts[2]) && $parts[2] > '') {
+            if (isset($parts[2]) && in_array($parts[2], $this->modelList)) {
                 $this->model = $parts[2];
             }
             
             if (count($parts) > 3) {
+                $this->model = 'base';
                 $this->view = 'lost';
             }
         }
