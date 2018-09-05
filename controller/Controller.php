be6fb9f7b4bff17412722c31147b190e1c61a91b
Professional <Professional@User-PC.Dlink>
2018-09-06 00:24:42 +0600

    lost page

--- controller/Controller.php
+++ controller/Controller.php
@@ -4,11 +4,11 @@
     {
         public $path;
         
-        public $view = 'home';
-        private $viewlList = ['home', 'lost'];
-        
         public $model = 'base';
         private $modelList = ['home', 'lost'];
+        
+        public $view = 'home';
+        private $viewlList = ['home', 'lost'];
                 
         function __construct($path = '/') {
             $this->path = $path;
@@ -17,11 +17,16 @@
         public function createRoute() {
             $parts = explode('/', $this->path);
             
-            if (isset($parts[1]) && in_array($parts[1], $this->viewlList)) {
-                $this->view = $parts[1];
-            }
             if (isset($parts[2]) && in_array($parts[2], $this->modelList)) {
                 $this->model = $parts[2];
+            } else {
+                $this->model = 'base';
+            }
+            
+            if (isset($parts[1]) && in_array($parts[1], $this->viewlList)) {
+                $this->view = $parts[1];
+            } else {
+                $this->view = 'lost';
             }
             
             if (count($parts) > 3) {
