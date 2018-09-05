34920463c115c61f97c5420f871edb21f62b80db
Professional <Professional@User-PC.Dlink>
2018-09-06 02:40:48 +0600

    lost view

--- view/lost.php
+++ view/lost.php
@@ -5,10 +5,12 @@
     </head>
 
     <body>
-        <?php include('Layout_navbar.php'); ?>
+        <div>
+            <?php include('Layout_navbar.php'); ?>
+        </div>
         
         <div class="container-fluid">
-            <div class="card" style="width: 15rem;">
+            <div class="card" style="width: 18rem;">
                 <img class="card-img-top" src="http://1001goroskop.ru/img/cards/koloda/56.jpg" width="170" height="300" alt="0">
                 <div class="card-body">
                     <h5 class="card-title">Nothing</h5>
