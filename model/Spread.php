a46a3dc39a2935fb398316fa7df867e8cc660c95
FalseShaman <open12Box>
2018-09-07 02:30:30 +0700

    ishtar-travel

--- model/Spread.php
+++ model/Spread.php
@@ -16,33 +16,12 @@
             for ($placeNumber=0; $placeNumber < $this->divCount; $placeNumber++)
             {
                 $map .= '<div class="col-md-'.$this->divSize.'" id="cardPlace'.$placeNumber.'">';
-
-                $placeFound = false;
                 foreach($positions as $place => $data)
                 {
                     if ($placeNumber == $data['position']) {
-                        $map .= '<div class="card" style="width: 11rem;">
-                            <img class="card-img-top" src="http://1001goroskop.ru/img/cards/koloda/57.jpg" width="85" height="150" alt="0">
-                            <div class="card-body">
-                                <h5 class="card-title">'.$place.'</h5>
-                                <p class="card-text">...</p>
-                            </div>
-                        </div>';
-
-                        $placeFound = true;
+                        $map .= '<p>'.$place.'</p><img class="card-img-top" src="http://1001goroskop.ru/img/cards/koloda/57.jpg" width="60" height="150" alt="0">';
                     }
                 }
-
-                if (!$placeFound) {
-                    $map .= '<div class="card" style="width: 11rem;">
-                            <img class="card-img-top" src="" width="85" height="150" alt="0">
-                            <div class="card-body">
-                                <h5 class="card-title"></h5>
-                                <p class="card-text"></p>
-                            </div>
-                        </div>';
-                }
-
                 $map .= '</div>';
             }
             $map .= '</div>';
