d2eb4da10ef43be39ba06580b9c4ae482129c413
FalseShaman <open12Box>
2018-09-06 20:58:29 +0700

    ishtar-travel

--- model/spread_ishtar-travel.php
+++ model/spread_ishtar-travel.php
@@ -1,9 +1,9 @@
 <?php 
     $rowCount = 5;
     $colCount = 4;
-    $cardCount = 15;
     
     $divSize = intval(12 / $rowCount);
+    $divCount = $rowCount * $colCount;
     
     $spread = array(
         'Инанна' => array('position' => 1),
@@ -24,35 +24,37 @@
     );
     
     $map = '<div class="row">';
-    for ($row=0; $row<$rowCount; $row++)
+    for ($placeNumber=0; $placeNumber<$divCount; $placeNumber++)
     {
-        for ($col=0; $col<$colCount; $col++)
+        $map .= '<div class="col-md-'.$divSize.'" id="cardPlace'.$placeNumber.'">';
+
+        $placeFound = false;
+        foreach($spread as $place => $data)
         {
-            $placeNumber = $col + $col*$row;
-            $map .= '<div class="col-md-'.$divSize.'" id="cardPlace'.$placeNumber.'">';
-            
-            $placeFound = false;
-            foreach($spread as $place => $data)
-            {
-                if ($placeNumber == $data['position']) {
-                    $map .= '<div class="card" style="width: 13rem;">
-                                <img class="card-img-top" src="http://1001goroskop.ru/img/cards/koloda/56.jpg" width="170" height="300" alt="0">
-                                <div class="card-body">
-                                    <h5 class="card-title">'.$place.'</h5>
-                                    <p class="card-text">...</p>
-                                </div>
-                            </div>';
-                    
-                    $placeFound = true;
-                }
+            if ($placeNumber == $data['position']) {
+                $map .= '<div class="card" style="width: 13rem;">
+                            <img class="card-img-top" src="http://1001goroskop.ru/img/cards/koloda/57.jpg" width="170" height="300" alt="0">
+                            <div class="card-body">
+                                <h5 class="card-title">'.$place.'</h5>
+                                <p class="card-text">...</p>
+                            </div>
+                        </div>';
+
+                $placeFound = true;
             }
-            
-            if (!$placeFound) {
-                
-            }
-            
-            $map .= '</div>';
         }
+
+        if (!$placeFound) {
+            $map .= '<div class="card" style="width: 13rem;">
+                            <img class="card-img-top" src="http://1001goroskop.ru/img/cards/koloda/56.jpg" width="170" height="300" alt="0">
+                            <div class="card-body">
+                                <h5 class="card-title"></h5>
+                                <p class="card-text"></p>
+                            </div>
+                        </div>';
+        }
+
+        $map .= '</div>';
     }
     $map .= '</div>';
 ?>
