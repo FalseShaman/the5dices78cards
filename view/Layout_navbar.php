afd6c0c79ade7ed8fe1dc700e997bde2bbb35867
FalseShaman <open12Box>
2018-09-07 00:43:50 +0700

    ishtar-travel

--- view/Layout_navbar.php
+++ view/Layout_navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="position: fixed; bottom: 0; width: 100%; height: 15%;">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="position: fixed; top: 0; width: 100%; height: 15%; font-size: 2em;   ">
     <ul class="navbar-nav">
         <?php echo $navbar; ?>
     </ul>
