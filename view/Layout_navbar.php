75e7fbd5bc9853dc59775a6e5004c53514a1e209
FalseShaman <open12Box>
2018-09-06 21:21:12 +0700

    ishtar-travel

--- view/Layout_navbar.php
+++ view/Layout_navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="position: absolute; bottom: 0;">
     <ul class="navbar-nav">
         <?php echo $navbar; ?>
     </ul>
