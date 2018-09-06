5658d8881ff0f640f7d82720b5b5d56433ac7cbe
FalseShaman <open12Box>
2018-09-07 00:44:25 +0700

    ishtar-travel

--- model/Spread.php
+++ model/Spread.php
@@ -22,7 +22,7 @@
                 {
                     if ($placeNumber == $data['position']) {
                         $map .= '<div class="card" style="width: 11rem;">
-                            <img class="card-img-top" src="http://1001goroskop.ru/img/cards/koloda/57.jpg" width="40" height="70">
+                            <img class="card-img-top" src="http://1001goroskop.ru/img/cards/koloda/57.jpg" width="85" height="150" alt="0">
                             <div class="card-body">
                                 <h5 class="card-title">'.$place.'</h5>
                                 <p class="card-text">...</p>
@@ -35,7 +35,7 @@
 
                 if (!$placeFound) {
                     $map .= '<div class="card" style="width: 11rem;">
-                            <img class="card-img-top" src="" width="40" height="70">
+                            <img class="card-img-top" src="" width="85" height="150" alt="0">
                             <div class="card-body">
                                 <h5 class="card-title"></h5>
                                 <p class="card-text"></p>
