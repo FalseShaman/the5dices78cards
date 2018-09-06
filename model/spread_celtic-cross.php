06dfbbcfe85491cdf4174cd3fa333581bb805c77
FalseShaman <open12Box>
2018-09-07 04:31:08 +0700

    celtic cross

--- model/spread_celtic-cross.php
+++ model/spread_celtic-cross.php
@@ -1,15 +1,15 @@
 <?php
     $positions = array(
-        'Суть проблемы' => array('position' => 25),
-        'Дополнительный фактор' => array('position' => 26),
+        'Суть проблемы' => array('position' => 13),
+        'Дополнительный фактор' => array('position' => 14),
         'Цели и желания' => array('position' => 1),
-        'Корень проблемы' => array('position' => 37),
-        'Прошлое' => array('position' => 24),
-        'Будущее' => array('position' => 27),
-        'Позиция кверента' => array('position' => 40),
-        'Окружение' => array('position' => 28),
-        'Страхи и надежды' => array('position' => 16),
-        'Итог' => array('position' => 3)
+        'Корень проблемы' => array('position' => 25),
+        'Прошлое' => array('position' => 12),
+        'Будущее' => array('position' => 15),
+        'Позиция кверента' => array('position' => 28),
+        'Окружение' => array('position' => 16),
+        'Страхи и надежды' => array('position' => 4),
+        'Итог' => array('position' => 5)
     );
 
     require 'Spread.php';
