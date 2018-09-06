b25a4111b535faada420406ff6788d78420bf918
FalseShaman <open12Box>
2018-09-07 02:36:28 +0700

    ishtar-travel

--- model/Spread.php
+++ model/Spread.php
@@ -15,11 +15,12 @@
             $map = '<div class="row">';
             for ($placeNumber=0; $placeNumber < $this->divCount; $placeNumber++)
             {
-                $map .= '<div class="col-md-'.$this->divSize.'" id="cardPlace'.$placeNumber.'">';
+                $map .= '<div class="col-md-'.$this->divSize.'" id="cardPlace'.$placeNumber.'" style="border: solid 1px; border-radius: 7px;">';
                 foreach($positions as $place => $data)
                 {
                     if ($placeNumber == $data['position']) {
-                        $map .= '<p>'.$place.'</p><img class="card-img-top" src="http://1001goroskop.ru/img/cards/koloda/57.jpg" width="60" height="150" alt="0">';
+                        $map .= '<p style="text-align: center; font-weight: bold;">'.$place.'</p>';
+                        $map .= '<img src="http://1001goroskop.ru/img/cards/koloda/57.jpg" width="60" height="150" alt="0">';
                     }
                 }
                 $map .= '</div>';
