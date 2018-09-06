a465b50482c8c9cebba45636ad6e976b2b9f18da
FalseShaman <open12Box>
2018-09-06 18:11:09 +0700

    fix font

--- view/Layout_head.php
+++ view/Layout_head.php
@@ -1,4 +1,4 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
 <title><?php echo $title; ?></title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <link href="public/css/simple-sidebar.css" rel="stylesheet">
