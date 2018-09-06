65d71fcb75f5cfff39f72ad99fad1688f080ce0d
FalseShaman <open12Box>
2018-09-07 03:36:00 +0700

    ishtar-travel

--- model/Spread.php
+++ model/Spread.php
@@ -12,7 +12,7 @@
                 {
                     if ($placeNumber == $data['position']) {
                         $map .= '<img src="http://1001goroskop.ru/img/cards/koloda/57.jpg" width="60" height="150">';
-                        $map .= '<span style="text-align: center; font-size: 1.2em;">'.$place.'</span>';
+                        $map .= '<span style="text-align: center; font-size: 0.8em; word-break: break-all;">'.$place.'</span>';
 
                         $cardCount--;
                         break;
