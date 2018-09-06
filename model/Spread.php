e7c8fd59a6aea9d49e18cdf97610d3569e2b7e53
FalseShaman <open12Box>
2018-09-07 03:21:48 +0700

    ishtar-travel

--- model/Spread.php
+++ model/Spread.php
@@ -7,11 +7,11 @@
             $cardCount = count($positions);
             for ($placeNumber=0; $placeNumber < 78; $placeNumber++)
             {
-                $map .= '<div class="col-md-1" id="cardPlace'.$placeNumber.'" style="border: solid 1px; border-radius: 7px;">';
+                $map .= '<div class="col-md-1" id="cardPlace'.$placeNumber.'" style="border: solid 1px; border-radius: 7px; height: 250px;">';
                 foreach($positions as $place => $data)
                 {
                     if ($placeNumber == $data['position']) {
-                        $map .= '<p style="text-align: center; font-weight: bold;">'.$place.'</p>';
+                        $map .= '<span style="text-align: center; font-weight: bold;">'.$place.'</span>';
                         $map .= '<img src="http://1001goroskop.ru/img/cards/koloda/57.jpg" width="60" height="150" style="position: absolute; bottom: 15px;">';
 
                         $cardCount--;
