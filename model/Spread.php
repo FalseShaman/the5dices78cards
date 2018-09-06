eff87418264f1b5b46fa8b14e4b88e2c47059002
FalseShaman <open12Box>
2018-09-07 02:49:07 +0700

    ishtar-travel

--- model/Spread.php
+++ model/Spread.php
@@ -13,17 +13,25 @@
 
         public function getMap($positions) {
             $map = '<div class="row">';
+            $cardCount = count($positions);
             for ($placeNumber=0; $placeNumber < 78; $placeNumber++)
             {
                 $map .= '<div class="col-md-1" id="cardPlace'.$placeNumber.'" style="border: solid 1px; border-radius: 7px;">';
                 foreach($positions as $place => $data)
                 {
                     if ($placeNumber == $data['position']) {
-                        $map .= '<p style="text-align: center; font-weight: bold;">'.$place.'</p>';
-                        $map .= '<img src="http://1001goroskop.ru/img/cards/koloda/57.jpg" width="60" height="150" style="margin-top: 15px;">';
+                        $map .= '<p style="text-align: center; font-weight: bold; position: relative; top: 15px; z-index: 5;">'.$place.'</p>';
+                        $map .= '<img src="http://1001goroskop.ru/img/cards/koloda/57.jpg" width="60" height="150" style="position: relative; bottom: 15px;">';
+
+                        $cardCount--;
+                        break;
                     }
                 }
                 $map .= '</div>';
+
+                if ($cardCount == 0) {
+                    break;
+                }
             }
             $map .= '</div>';
 
