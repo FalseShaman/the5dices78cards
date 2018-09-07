1565f385d4bb3396aa9326655455c8aebcb7073e
FalseShaman <open12Box>
2018-09-07 22:16:23 +0700

    ajax + lavecraft

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
 
 //    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if ($path == '/getDeckList') {
-            return $controller->getDeckList();
+            var_dump($controller->getDeckList());
         }
 //    }
 ?>
