d7ccd3d81b887f881feebc6ee6a6374d1256a2e0
FalseShaman <open12Box>
2018-09-07 22:18:12 +0700

    ajax + lavecraft

--- index.php
+++ index.php
@@ -19,18 +19,18 @@
     require_once 'controller/Controller.php';
     $controller = new Controller($path);
 
-    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-        $title = $controller->getTitle(getTranslate());
-        $navbar = $controller->getNav(getTranslate());
-        $content = '';
-
-        if (!$controller->subpage) {
-            $controller->subpage = 'basic';
-        }
-
-        require 'view/'.ucfirst($controller->page).'/'.$controller->page.'_'.$controller->subpage.'.php';
-        include_once('view/layout.php');
-    }
+//    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+//        $title = $controller->getTitle(getTranslate());
+//        $navbar = $controller->getNav(getTranslate());
+//        $content = '';
+//
+//        if (!$controller->subpage) {
+//            $controller->subpage = 'basic';
+//        }
+//
+//        require 'view/'.ucfirst($controller->page).'/'.$controller->page.'_'.$controller->subpage.'.php';
+//        include_once('view/layout.php');
+//    }
 
 //    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if ($path == '/getDeckList') {
