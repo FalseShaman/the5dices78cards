d0bfcc9264eb4cac99eea96293ab948e5ff3d0f6
FalseShaman <open12Box>
2018-09-10 13:47:32 +0700

    save way

--- index.php
+++ index.php
@@ -46,50 +46,44 @@
     require_once 'model/controller.php';
     $controller = new Controller($path);
 
-//    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-//        $title = $controller->getTitle(getTranslate());
-//        $leftMenu = $controller->getNav(getTranslate());
-//        $content = '';
-//
-//        if (!$controller->subpage) {
-//            $controller->subpage = 'basic';
-//        }
-//
-//        require 'view/'.ucfirst($controller->page).'/'.$controller->page.'_'.$controller->subpage.'.php';
-//        include_once('view/layout.php');
-//    }
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        $title = $controller->getTitle(getTranslate());
+        $leftMenu = $controller->getNav(getTranslate());
+        $content = '';
+
+        if (!$controller->subpage) {
+            $controller->subpage = 'basic';
+        }
+
+        require 'view/'.ucfirst($controller->page).'/'.$controller->page.'_'.$controller->subpage.'.php';
+        include_once('view/layout.php');
+    }
 
 //    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 //        if ($path == '/save/spread') {
 //            $controller->saveSpread();
 //        }
 //    }
-$data = extract(parse_url($_ENV["DATABASE_URL"]));
-var_dump($data);
 
-$dsn = "pgsql:"
-    . "host=ec2-54-217-245-9.eu-west-1.compute.amazonaws.com;"
-    . "dbname=daabdc45roinq9;"
-    . "user=xxruwosifumind;"
-    . "port=5432;"
-    . "sslmode=require;"
-    . "password=76c9995e9184ee542182e0e34f3355898b82ec454aea38ff676d298c913d5da6";
+    $dsn = "pgsql:"
+        . "host=ec2-54-217-245-9.eu-west-1.compute.amazonaws.com;"
+        . "dbname=daabdc45roinq9;"
+        . "user=xxruwosifumind;"
+        . "port=5432;"
+        . "sslmode=require;"
+        . "password=76c9995e9184ee542182e0e34f3355898b82ec454aea38ff676d298c913d5da6";
 
-$db = new PDO($dsn);
+    $db = new PDO($dsn);
 
     $db->exec('CREATE TABLE IF NOT EXISTS spread (
                            id SERIAL PRIMARY KEY,
                            title CHARACTER (200) NOT NULL,
                            map CHARACTER (250) NOT NULL');
-
-    var_dump($controller->getTables());
-
     if($db){
         echo "Connected <br />".$db;
     }else {
         echo "Not connected";
     }
-
     $data = $db->query("SELECT table_name 
                                        FROM information_schema.tables 
                                        WHERE table_schema= 'public' 
