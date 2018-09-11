aaf679cff061b0766102f4395d46ca110ee4d896
FalseShaman <open12Box>
2018-09-11 19:04:39 +0700

    auth

--- model/user.php
+++ model/user.php
@@ -28,8 +28,8 @@
 
         public function save() {
             $folder = md5($this->pass.$this->username);
-            return 'INSERT INTO user (name, pass, folder, register) VALUES ("'.$this->username.'", "'.$this->pass.'", "'.$folder.'")';
-//            $insert = connection::runQuery('INSERT INTO user (name, pass, folder, register) VALUES ('.$this->username.', '.$this->pass.', '.$folder.', NOW())');
+            $insert = connection::runQuery('INSERT INTO user (name, pass, folder, register) VALUES ('.$this->username.', '.$this->pass.', '.$folder.', NOW())');
+            return $insert;
 //            if ($insert['status']) {
 //                return $this->getOne();
 //            } else {
