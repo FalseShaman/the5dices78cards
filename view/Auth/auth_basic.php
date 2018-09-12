3170baf10323b003f750a9d498a7db11d14397b7
FalseShaman <open12Box>
2018-09-12 21:19:29 +0700

    auth

--- view/Auth/auth_basic.php
+++ view/Auth/auth_basic.php
@@ -8,13 +8,21 @@
                 
                     <div class="form-group col-md-4">
                         <label for="pass" class="form-control">Пароль:</label>
+                    </div>
+                    <div class="col-md-8"></div>
+                    
+                    <div class="form-group col-md-2">
                         <input type="text" class="form-control" id="pass" autocomplete="off">
+                    </div>
+                    <div class="form-group col-md-2">
                         <input type="text" class="form-control" id="pass_again" autocomplete="off">
                     </div>
                     <div class="col-md-8"></div>
                 
-                    <div class="form-group col-md-4">
+                    <div class="form-group col-md-2">
                         <button type="button" class="form-control btn btn-success" id="login" disabled>Войти</button>
+                    </div>
+                    <div class="form-group col-md-2">
                         <button type="button" class="form-control btn btn-info" id="register" disabled>Зарегистрироваться</button>
                     </div>
                     <div class="col-md-8"></div>
