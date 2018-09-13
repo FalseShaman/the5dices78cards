09af970ae74cf5f45e36afd54a5f21a26e9c7b2b
FalseShaman <open12Box>
2018-09-13 17:23:00 +0700

    spread

--- model/view/Spread/spread_open.php
+++ model/view/Spread/spread_open.php
@@ -18,7 +18,7 @@
             $map = json_decode($li['map']);
             $leftMenu .= '<li class="nav-item"><span id="spread'.$li['id'].'" style="display: none;">';
             foreach ($map as $place) {
-                $leftMenu .= '<span data-place="'.$place->place.'" data-arcana="'.$place->arcana.'" data-position="'.$place->position.'">';
+                $leftMenu .= '<span data-place="'.$place->place.'" data-arcana="'.$place->arcana.'" data-position="'.$place->position.'"></span>';
             }
             $leftMenu .= '</span><button class="btn btn-dark spreadSelectButton" id="'.$li['id'].'" style="margin: 10px 0;">'.$li['title'].'</button></li>';
         }
