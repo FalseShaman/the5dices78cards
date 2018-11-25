d826ee2368482f22a5e2032fd6c86965f4f79efb
falseshaman <falseshaman@gmail.com>
2018-11-25 11:57:08 -0500

    spread creator, main css

--- view/Spread/spread_basic.php
+++ view/Spread/spread_basic.php
@@ -1,16 +1,16 @@
 <?php
     $form = '';
     $form = '<div class="col-md-2">
-                <label class="form-control" for="height">Высота:</label>
-                <input class="form-control" type="number" id="height">
+                <label for="height">Высота:</label>
+                <input type="number" id="height">
             </div>
             <div class="col-md-2">
-                <label class="form-control" for="length">Длина:</label>
-                <input class="form-control" type="number" id="length">
+                <label for="length">Длина:</label>
+                <input type="number" id="length">
             </div>
             <div class="col-md-4">
-                <label class="form-control" for="title">Название:</label>
-                <input class="form-control" type="text" id="title">
+                <label for="title">Название:</label>
+                <input type="text" id="title">
             </div>
             <div class="col-md-2">
                 <button class="form-control" id="createSpread">Создать</button>
