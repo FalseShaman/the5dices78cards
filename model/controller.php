8347402b6d9e70bdb0c43e93ec0b9b96c40198e6
FalseShaman <falseshaman@gmail.com>
2018-11-25 09:55:47 +0000

    fix login

--- model/controller.php
+++ model/controller.php
@@ -60,8 +60,7 @@
             require_once 'user.php';
             $user = new User($_POST['name'], $_POST['pass']);
             $userData = $user->auth();
-            var_dump($userData); die();
-            if ($userData->id) {
+            if ($userData) {
                 return array('status' => 'done');
             } else {
                 return array('status' => 'fail', 'message' => 'Ошибка авторизации');
