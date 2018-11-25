a5591609fbe6717e34505b567db0cf74453fc550
FalseShaman <falseshaman@gmail.com>
2018-11-25 10:27:04 +0000

    profile

--- view/Profile/profile_basic.php
+++ view/Profile/profile_basic.php
@@ -9,11 +9,11 @@
         $rightMenu .= '<li class="nav-item"><button class="btn btn-light" href="javascript:void(0);" id="'.$spreadLi->id.'" style="margin: 10px 0;">'.$spreadLi->title.'</button></li>';
     }
 
-    $openButton = '<button type="button" class="btn btn-light" id="profileEditButton" title="Редактировать" style="position: fixed; top: 55px; right: 12%;"><img class="img-responsive" src="/view/design/edit.png"></button>';
-    $saveButton = '<button type="button" class="btn btn-light" id="profileSaveButton" title="Сохранить" style="position: fixed; top: 105px; right: 12%;"><img class="img-responsive" src="/view/design/save.png"></button>';
+    $openButton = '<button type="button" class="btn btn-light" id="profileEditButton" title="Редактировать" style="position: fixed; top: 55px; left: 12%;"><img class="img-responsive" src="/view/design/edit.png"></button>';
+    $saveButton = '<button type="button" class="btn btn-light" id="profileSaveButton" title="Сохранить" style="position: fixed; top: 105px; left: 12%;"><img class="img-responsive" src="/view/design/save.png"></button>';
     $controlPanel .= $openButton.$saveButton;
 
-    $profile = '<div class="row">';
+    $profile = '<div class="row" style="background: #CCCCB4; color: #1B2743; border-radirus: 10px;">';
     $profile .= '<div class="col-md-4"><h2>'.$user->name.'</h2></div>';
     $profile .= '<div class="col-md-8"><p>'.$user->rules.'</p></div>';
     $profile .= '<div class="col-md-4"><h3>'.$user->specialization.'</h3></div>';
