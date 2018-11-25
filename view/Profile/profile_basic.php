940239f856bf1ced047dbc5a07c2c3381fe31018
falseshaman <falseshaman@gmail.com>
2018-11-25 10:20:36 -0500

    show profile

--- view/Profile/profile_basic.php
+++ view/Profile/profile_basic.php
@@ -16,8 +16,12 @@
     $profile = '<div class="row" style="background: #CCCCB4; color: #1B2743; border-radirus: 10px;">';
     $profile .= '<div class="col-md-4"><h2>'.$user->name.'</h2></div>';
     $profile .= '<div class="col-md-8"><p>'.$user->rules.'</p></div>';
+    $profile .= '<div class="col-md-2"><p>'.$user->register.'</p></div>';
+    $profile .= '<div class="col-md-10"></div>';
     $profile .= '<div class="col-md-4"><h3>'.$user->specialization.'</h3></div>';
-    $profile .= '<div class="col-md-8"><p>'.$user->decks.'</p></div>';
+    $profile .= '<div class="col-md-8"></div>';
+    $profile .= '<div class="col-md-4"><p>'.$user->decks.'</p></div>';
+    $profile .= '<div class="col-md-8"></div>';
     $profile .= '</div>';
 
     $content = $controlPanel.$profile;
