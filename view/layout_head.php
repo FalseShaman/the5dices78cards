37f4830931674530fd6bd18141dac3ccaf38e29b
FalseShaman <open12Box>
2018-11-26 19:59:51 +0700

    style file

--- view/layout_head.php
+++ view/layout_head.php
@@ -1,4 +1,4 @@
 <meta charset="utf-8">
 <title><?php echo $title; ?></title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="design/css/main.css">
+<link rel="stylesheet" type="text/css" href="view/design/css/main.css">
