15f43575f730a1392d20dbaaddf75091031ac584
FalseShaman <open12Box>
2018-12-01 04:42:32 +0700

    fix modals

--- view/Spread/spread_basic.php
+++ view/Spread/spread_basic.php
@@ -7,9 +7,9 @@
 
     $content = '<div class="col-md-12 spreadMap" id="spreadMap">';
     
-    inlcude '/app/model/Spread/modals/spread_create.php' ;
-    include '/app/model/Spread/modals/spread_list.php';
-    include '/app/model/Spread/modals/spread_position.php';
+    inlcude "/app/model/Spread/modals/spread_create.php" ;
+    include "/app/model/Spread/modals/spread_list.php";
+    include "/app/model/Spread/modals/spread_position.php";
 
     $newButton = '<button type="button" class="btn btn-light" id="spreadCreate" title="Создать"><img class="img-responsive" src="/view/design/new.png"></button>';
     $listButton = '<button type="button" class="btn btn-light" id="spreadList" title="Список"><img class="img-responsive" src="/view/design/open.png"></button>';
