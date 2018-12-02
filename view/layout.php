ded2f4fc0472a231bc9edd67911f9d33c318ba2f
FalseShaman <open12Box>
2018-12-03 02:46:28 +0700

    horizontal

--- view/layout.php
+++ view/layout.php
@@ -5,28 +5,28 @@
     </head>
 
     <body>
-        <div class="container-fluid">
-            <div class="topPanel">
-                <h2>ordo draco sacerdos librarium</h2>
-            </div>
+        <div class="topPanel">
+            <h2>ordo draco sacerdos librarium</h2>
+        </div>
 
-            <?php if($leftMenu && $leftMenu > ''): ?>
+        <?php if($leftMenu && $leftMenu > ''): ?>
+        <nav class="navbar navbar-dark bg-dark">
+            <ul class="navbar-nav">
+                <?php echo $leftMenu; ?>
+            </ul>
+        </nav>
+        <?php endif; ?>
+
+        <?php if($rightMenu && $rightMenu > ''): ?>
             <nav class="navbar navbar-dark bg-dark">
                 <ul class="navbar-nav">
-                    <?php echo $leftMenu; ?>
+                    <li class="nav-item"><button type="button" class="btn btn-light" id="backChange"><img class="img-responsive" src="/view/design/refresh.png"></button></li>
+                    <?php echo $rightMenu; ?>
                 </ul>
             </nav>
-            <?php endif; ?>
+        <?php endif; ?>     
 
-            <?php if($rightMenu && $rightMenu > ''): ?>
-                <nav class="navbar navbar-dark bg-dark">
-                    <ul class="navbar-nav">
-                        <li class="nav-item"><button type="button" class="btn btn-light" id="backChange"><img class="img-responsive" src="/view/design/refresh.png"></button></li>
-                        <?php echo $rightMenu; ?>
-                    </ul>
-                </nav>
-            <?php endif; ?>     
-
+        <div class="container-fluid">
             <div class="container">
                 <?php echo $content; ?>
             </div>
