9179aef4945bdf21c27ca35c97addc51ce05ce2d
FalseShaman <open12Box>
2018-12-04 05:13:36 +0700

    new profile

--- view/Profile/profile_basic.php
+++ view/Profile/profile_basic.php
@@ -11,9 +11,9 @@
     $profile = '<form class="profileForm"> 
                     <div class="col-sm-5">
                         <h2>'.$user->name.'</h2>
-                        <div class="col-sm-5">'.$specializationSelect.'</div>
+                        <div>'.$specializationSelect.'</div>
                     </div>
-                    <div class="col-sm-6"><textarea id="profileRules" placeholder="12 кредо">'.$user->rules.'</textarea></div>
+                    <div class="col-sm-5"><textarea id="profileRules" placeholder="12 кредо">'.$user->rules.'</textarea></div>
                     <div class="col-sm-11"><input type="text" id="profileDecks" value="'.$user->decks.'" placeholder="Используемые колоды"></div>
                 </form>';
 
