bd58b73899253ad5814dcceeb30b7768e67ea176
FalseShaman <open12Box>
2018-12-05 04:03:44 +0700

    login fix

--- view/Auth/auth_basic.php
+++ view/Auth/auth_basic.php
@@ -1,10 +1,10 @@
 <?php
     $content = '<div class="row authForm">
                     <div class="form-group col-sm-4">
-                        <input type="text" class="form-control" id="name" autocomplete="off" placeholder="name">
+                        <input type="text" class="form-control" id="name" placeholder="name">
                     </div>
                     <div class="form-group col-sm-4">
-                        <input type="password" class="form-control" id="pass" autocomplete="off" placeholder="pass">
+                        <input type="password" class="form-control" id="pass" placeholder="pass">
                     </div>
                     <div class="form-group col-sm-4">
                         <button type="button" class="form-control btn btn-info" id="login">Yau!</button>
