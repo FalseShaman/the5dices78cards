0cd2874e80203bec49e5676b5bb076c04bcf500f
FalseShaman <open12Box>
2018-12-13 15:39:02 +0700

    table

--- view/layout_script.php
+++ view/layout_script.php
@@ -2,19 +2,4 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
-<!-- BackgroundUpdate -->
-<!-- <script>
-    var backLine = "php echo $backLine; ?>";
-    var backList = backLine.split('|');
-    $.each(backList, function(ind, val){
-        var pic = new Image();
-        pic.src = '/view/design/background/'+val;
-    });
-
-    $('#backChange').click(function(){
-        number = Math.floor((Math.random() * backList.length));
-        $('body').attr('style', 'background-image: url("/view/design/background/'+backList[number]+'"); background-size: 100%;');
-    });
-</script> -->
-
 <script src="view/<?php echo ucfirst($controller->page); ?>/js/script.js"></script>
